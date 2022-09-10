1a47c7799294c8ddf2cb7a9c5c031591acc9b6dd
dibala21 <dielrosen98@gmail.com>
2022-09-09 20:52:34 -0400

    oublie de push

--- Bubble.cpp
+++ Bubble.cpp
@@ -4,6 +4,7 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 #include "Bubble.h"
+#include <vector>
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -47,30 +48,38 @@ Bubble::~Bubble()
 ///////////////////////////////////////////////////////////////////////////////
 void Bubble::thread(void)
 {
-	// Variable
+	const unsigned int ADDR_MEM_SIZE = 0;
 
-	
+	// Variables
+	std::vector<unsigned int> unsorted_data;
+	int mem_size;
+
 	// 1ere lecture: nombre d'éléments à trier
 	/*
-	
+
 	À compléter
-	
+
 	*/
+	mem_size = readPort.Read(ADDR_MEM_SIZE);
 
 	// Lecture des éléments à trier
 	/*
-	
+
 	À compléter
-	
+
 	*/
+	for (int i = 1; i <= mem_size; i++) {
+		unsorted_data.push_back(readPort.Read(i));
+	}
 
 	//Appel à bubble sort
 	/*
-	
+
 	À compléter
-	
+
 	*/
-		
+	bubbleSort(unsorted_data, mem_size);
+
 	// Arrêt de la simulation
 	sc_stop();
 
@@ -86,22 +95,31 @@ void Bubble::bubbleSort(unsigned int *ptr, int counter)
 {
 	// Affichage avant tri
 	/*
-	
+
 	À compléter
-	
+
 	*/
+	printf("Unsorted data input:\n");
+	for (int i = 0; i < counter; i += 4) {
+		printf("%d: %d %d %d %d\n", counter, ptr[i], ptr[i + 1], ptr[i + 2], ptr[i + 3]);
+	}
 
 	// Tri
 	/*
-	
+
 	À compléter
-	
+
 	*/
-	
+
+
 	// Affichage après tri
 	/*
-	
+
 	À compléter
-	
+
 	*/
+	printf("Sorted data output (ascending):\n");
+	for (int i = 0; i < counter; i += 4) {
+		printf("%d: %d %d %d %d\n", counter, ptr[i], ptr[i + 1], ptr[i + 2], ptr[i + 3]);
+	}
 }
